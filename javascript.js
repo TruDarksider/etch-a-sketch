2f567da413fcf37fcaa33ef3d23a01fbcfea57bd
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-08-16 16:35:45 -0500

    Added colorSquare and addHoverEvent functions

--- javascript.js
+++ javascript.js
@@ -1,18 +1,33 @@
+const body = document.querySelector('body');
+const grid = document.createElement('div');
+const gridSquare = document.createElement('div');
+
 function createGrid(){
-    const body = document.querySelector('body');
-    const grid = document.createElement('div');
     grid.classList.add('grid');
     body.appendChild(grid);
 }
 
 function addGridSquares(numSquares){
-    const grid = document.querySelector('.grid');
-    for(let i=0; i<numSquares; i++){
+    for(let i=0; i<(numSquares*numSquares); i++){
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('gridSquare');
-        gridSquare.textContent = " ";
-        grid.appendChild(gridSquare);}
+        //gridSquare.textContent = "";
+        grid.appendChild(gridSquare);
+    }
+}
+
+function colorSquare(gridSquare){
+    gridSquare.classList.add('colored');
+}
+
+function addHoverEvent(){
+    grid.addEventListener('mouseover', function (e){
+        if (e.target.classList.contains('gridSquare')){
+            colorSquare(e.target);
+        }
+    })
 }
 
 createGrid();
 addGridSquares(16);
+addHoverEvent();
